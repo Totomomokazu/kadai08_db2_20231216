caa1420dd0d625049d5783815b97bef3ba080914
Totomomokazu <kanketomokazu@gmail.com>
2023-12-24 15:41:10 +0900

    detail処理の追加

--- detail.php
+++ detail.php
@@ -1,3 +1,22 @@
+<!-- このdetailファイルで各データを操作する -->
+
+<!-- ここから下はphpの処理 -->
 <?php
 
 ?>
+
+
+
+
+<!-- ここから下はhtml -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
